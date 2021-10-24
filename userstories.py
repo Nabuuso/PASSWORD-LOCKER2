67fffb7f9ac9b07f6d3b8af47fd7d8e643cb8de8
betty <betty.nabuuso@moringaschool.com>
2021-10-24 19:00:35 +0300

    added class user account

--- userstories.py
+++ userstories.py
@@ -1,2 +1,12 @@
-username = input("Enter username:")
-print("Username is: " + username)
+class useraccount:
+    """
+    Class that generates new instances of users.
+    """
+    account_list = [] #empty account list
+    
+    def __init__(self,account_name,user_name,password,email):
+         self.account_name = account_name
+         self.user_name = user_name
+         self.password = password
+         self.email = email
+        
