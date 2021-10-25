bdacba06a26e7608f55940c32c4a2c9a6010d0b7
betty <betty.nabuuso@moringaschool.com>
2021-10-25 08:38:13 +0300

    added credential file

---
+++ credentials.py
@@ -0,0 +1,20 @@
+class Credentials:
+    """
+    Class that will generate new instances of credentials
+    """
+
+    credentials_detail = []
+
+    def __init__(self,credentials_detail):
+
+        """
+        the __init__method helps us define properties for our objects.
+        """
+
+
+        self.credentials_detail = credentials_detail
+
+
+        """
+        arguments for our __init__method will include the following.
+        """
--- user.py
+++ user.py
@@ -1,16 +1,16 @@
-class user:
-    """
-    Class that generates new instances of users.
-    """
-    user_list = [] #empty account list
-    def _init_ (self, user_name, password):
-        self.user_name = user_name
-        self.password = password
+# class user:
+#     """
+#     Class that generates new instances of users.
+#     """
+#     user_list = [] #empty account list
+#     def _init_ (self, user_name, password):
+#         self.user_name = user_name
+#         self.password = password
         
-    def save_user(self):
-        user.user_list.append(self)
+#     def save_user(self):
+#         user.user_list.append(self)
         
-    
+   
    
 
   
