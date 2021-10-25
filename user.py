6858f22988b667b396087d0d51833e36e153ab0d
betty <betty.nabuuso@moringaschool.com>
2021-10-25 08:04:17 +0300

    added credential file

--- user.py
+++ user.py
@@ -3,7 +3,7 @@ class user:
     Class that generates new instances of users.
     """
     user_list = [] #empty account list
-    def _init_ (self, user_name,password):
+    def _init_ (self, user_name, password):
         self.user_name = user_name
         self.password = password
         
