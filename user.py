15effb3922d4ced3dd3254c36c19ec5ad3e822f2
betty <betty.nabuuso@moringaschool.com>
2021-10-26 14:32:36 +0300

    modifying the user.py file

--- user.py
+++ user.py
@@ -1,13 +1,40 @@
+from typing import List
+
+
 class user:
     """
     Class that generates new instances of users.
     """
     user_list = [] #empty account list
-    def _init_ (self, user_name, password):
+    def __init__ (self, user_name, password):
         self.user_name = user_name
         self.password = password
         
-    def save_user(self):
+    def append_user(self):
+        """
+        method to save user in the list
+        """
         user.user_list.append(self)
+    
+    @classmethod
+    def display_data(cls):
+        """
+        display objects in the list
+        """
+        return user.user_list
         
-   
+        
+    @classmethod
+    def getSingleInstance(cls):
+        list = cls.user_list
+        for i in list:
+            print(i.__dict___)
+        
+
+x = user('Betty', "!@#$")
+x.append_user()
+print(x.user_name)
+print(x.user_list)
+y=x.display_data()
+print(y)
+x.getSingleInstance()
