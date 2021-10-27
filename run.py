005b97d99e7052aff40282c23479538e45b8449f
betty <betty.nabuuso@moringaschool.com>
2021-10-27 05:00:49 +0300

    running the main function

--- run.py
+++ run.py
@@ -2,80 +2,81 @@
 
 def main(): 
     while True:
-        print("sign in => To login to app")
-        print("x => To logout from app")
-        user_choice = input().lower()
-        if user_choice == 'signin':
-            print('welcome')
-        # print("welcome to password locker!!!")
-        # print('\n')
-        # print("select a short code to navigate through: to create new user use 'nu': To login to your account 'lg' to exit")
-        # short_code = input().lower()
-        # print('\n')
-    
-        # if short_code == 'nu':
-        #     print('create username')
-        #     created_user_name = input()
+        # print("sign in => To login to app")
+        # print("x => To logout from app")
+        # user_choice = input().lower()
+        # if user_choice == 'signin':
+        #     print('welcome')
             
-        #     print('create password')
-        #     created_user_password =input()
+        print("welcome to password locker!!!")
+        print('\n')
+        print("select a short code to navigate through: to create new user use 'nu': To login to your account 'lg' to exit")
+        short_code = input().lower()
+        print('\n')
+    
+        if short_code == 'nu':
+            print('create username')
+            created_user_name = input()
             
-        #     print('confirm password')
-        #     confirm_password =input
+            print('create password')
+            created_user_password =input()
+            
+            print('confirm password')
+            confirm_password =input
     
-        #     while confirm_password!= created_user_password:
-        #         print("invalid password did not match!!")
-        #         print("enter your password")
-        #         created_user_password =input()
-        #         print("confirm paswword")
-        #         confirm_password =input()
+            while confirm_password!= created_user_password:
+                print("invalid password did not match!!")
+                print("enter your password")
+                created_user_password =input()
+                print("confirm paswword")
+                confirm_password =input()
                 
-        #     else:
-        #         print(f"congratulations {created_user_name}! account creation successful")
-        #         print('\n')
-        #         print("proceed to login")
-        #         print("username")
-        #         entered_username = input()
-        #         print("your password")
-        #         entered_password = input()
+            else:
+                print(f"congratulations {created_user_name}! account creation successful")
+                print('\n')
+                print("proceed to login")
+                print("username")
+                entered_username = input()
+                print("your password")
+                entered_password = input()
                 
-        #     while entered_username!= created_user_name or entered_password!= created_user_password:
-        #         print("invalid username or password")
-        #         print("username")
-        #         entered_username = input()
-        #         print("your password")
-        #         entered_password = input()
+            while entered_username!= created_user_name or entered_password!= created_user_password:
+                print("invalid username or password")
+                print("username")
+                entered_username = input()
+                print("your password")
+                entered_password = input()
         
-        #     else:
-        #         print(f"welcome: {entered_username} to your account")
-        #         print('\n')
+            else:
+                print(f"welcome: {entered_username} to your account")
+                print('\n')
     
-        # elif short_code == 'lg': 
-        #     print("welcome")
-        #     print("Enter user name")
-        #     default_user_name =input()
-        #     print("Enter password")
-        #     default_user_password =input()
-        #     print('\n')
+        elif short_code == 'lg': 
+            print("welcome")
+            print("Enter user name")
+            default_user_name =input()
+            print("Enter password")
+            default_user_password =input()
+            print('\n')
     
-        #     while default_user_name!= 'testuser' or default_user_password!= '12345':
-        #         print("wrong userName or password.username 'testuser' and pasword '12345'")
-        #         print("Enter user name")
-        #         default_user_name =input()
-        #         print("Enter password")
-        #         default_user_password =input()
-        #         print('\n')
+            while default_user_name!= 'testuser' or default_user_password!= '12345':
+                print("wrong userName or password.username 'testuser' and pasword '12345'")
+                print("Enter user name")
+                default_user_name =input()
+                print("Enter password")
+                default_user_password =input()
+                print('\n')
         
-        #     else:
-        #         print("login success")
-        #         print('\n')
-        #         print('\n')
+            else:
+                print("login success")
+                print('\n')
+                print('\n')
         
-        # elif short_code =="ex":
-        #     break
+        elif short_code =="ex":
+            break
 
-        # else:
-        #     print("Enter valid code to continue")
+        else:
+            print("Enter valid code to continue")
     
-if __name__ == '_main_':
+if __name__ == '__main__':
     main()
